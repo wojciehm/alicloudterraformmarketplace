9a586932282bc5b010e43647b2e82e6eca640035
Wojciech Marusiak <register@wojcieh.net>
2018-10-29 22:08:14 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,130 @@
+provider "alicloud" {
+	  access_key = "${var.access_key}"
+	  secret_key = "${var.secret_key}"
+	  region     = "${var.region}"
+}
+##################VPC####################
+resource "alicloud_vpc" "main" {
+		name = "${var.vpc_name}"
+		cidr_block = "${var.vpc_cidr}"
+}
+############## vswitch ################
+resource "alicloud_vswitch" "main"{
+		vpc_id = "${alicloud_vpc.main.id}"
+		cidr_block = "${var.vswitch_cidr}"
+	  availability_zone = "${var.vpc_availability_zone}"
+	  depends_on = [
+	  "alicloud_vpc.main",
+	  ]
+}
+################### SG ###############
+resource "alicloud_security_group" "main" {
+		name = "${var.security_group}"
+		vpc_id = "${alicloud_vpc.main.id}"
+}
+
+################## SG rules ##############
+
+resource "alicloud_security_group_rule" "http-in" {
+	  type = "ingress"
+		ip_protocol = "tcp"
+	  nic_type  = "internet"
+		policy = "accept"
+	  port_range = "80/80"
+	  priority = 1
+	  security_group_id = "${alicloud_security_group.main.id}"
+	  cidr_ip = "0.0.0.0/0"
+	}
+resource "alicloud_security_group_rule" "ssh-in" {
+	  type = "ingress"
+	  ip_protocol = "tcp"
+	  nic_type = "internet"
+    policy = "accept"
+	  port_range = "22/22"
+	  priority = 1
+	  security_group_id = "${alicloud_security_group.main.id}"
+	  cidr_ip = "0.0.0.0/0"
+}
+############################Magento#######################
+
+data "alicloud_images" "ecs_image" {
+  #most_recent = "${var.most_recent}"
+  owners      = "${var.image_owners}"
+  name_regex  = "${var.name_regex}"
+}
+
+resource "alicloud_disk" "disk" {
+  availability_zone = "${var.vpc_availability_zone}"
+  category          = "${var.disk_category}"
+  size              = "${var.disk_size}"
+  count             = "${var.count}"
+}
+
+resource "alicloud_instance" "instance" {
+  instance_name     = "${var.role}"
+  host_name         = "${var.role}"
+  image_id					= "${var.ecs_id}"
+	instance_type     = "${var.ecs_type}"
+  count             = "${var.count}"
+  availability_zone = "${var.vpc_availability_zone}"
+  security_groups   = ["${alicloud_security_group.main.*.id}"]
+
+  internet_charge_type       = "${var.internet_charge_type}"
+  internet_max_bandwidth_out = "${var.internet_max_bandwidth_out}"
+
+  password = "${var.ecs_password}"
+
+  instance_charge_type = "PostPaid"
+  system_disk_category = "cloud_efficiency"
+
+  tags {
+    role = "${var.role}"
+    dc = "${var.datacenter}"
+  }
+}
+
+resource "alicloud_disk_attachment" "instance-attachment" {
+  count       = "${var.count}"
+  disk_id     = "${element(alicloud_disk.disk.*.id, count.index)}"
+  instance_id = "${element(alicloud_instance.instance.*.id, count.index)}"
+}
+################### RD ###########################
+
+resource "alicloud_db_instance" "instance" {
+  engine           = "${var.engine}"
+  engine_version   = "${var.engine_version}"
+  instance_type    = "${var.instance_class}"
+  instance_storage = "${var.storage}"
+  vswitch_id       = "${var.vswitch_id == "" ? alicloud_vswitch.main.id : var.vswitch_id}"
+}
+
+resource "alicloud_db_account" "account" {
+  count       = 1
+  instance_id = "${alicloud_db_instance.instance.id}"
+  name        = "tf_account"
+  password    = "${var.password}"
+}
+
+resource "alicloud_db_backup_policy" "backup" {
+  instance_id   = "${alicloud_db_instance.instance.id}"
+  backup_period = ["Tuesday", "Wednesday"]
+  backup_time   = "10:00Z-11:00Z"
+}
+
+resource "alicloud_db_connection" "connection" {
+  instance_id       = "${alicloud_db_instance.instance.id}"
+  connection_prefix = "tf-connection"
+}
+
+resource "alicloud_db_database" "db" {
+  count       = 1
+  instance_id = "${alicloud_db_instance.instance.id}"
+  name        = "${var.database_name}"
+}
+
+resource "alicloud_db_account_privilege" "privilege" {
+  count        = 1
+  instance_id  = "${alicloud_db_instance.instance.id}"
+  account_name = "${element(alicloud_db_account.account.*.name, count.index)}"
+  db_names     = ["${alicloud_db_database.db.*.name}"]
+}
---
+++ variables.tf
@@ -0,0 +1,135 @@
+######### Access Key ###############
+variable "access_key" {
+	  type = "string"
+	  default = ""
+}
+variable "secret_key" {
+	  type = "string"
+	  default = ""
+}
+variable "region" {
+	  type = "string"
+	  default = "eu-central-1"
+}
+################# VPC ############
+variable "vpc_id"{
+		description = "The VPC ID to launch vSwitch"
+		default = ""
+}
+variable "vpc_name"{
+		description = "VPC Name is TF"
+		default = "TerraformVPC"
+}
+variable "vpc_cidr" {
+		default  = "172.16.0.0/12"
+}	
+variable "vpc_region" {
+		default = "eu-central-1"
+}
+variable "vpc_availability_zone"{
+		default = "eu-central-1a"
+}
+########### vswitch ################
+variable "vswitch_id" {
+	  description = "The vswitch id used to launch one or more instances."
+	  default     = ""
+}
+variable "vswitch_name" {
+	  description = "The vswitch name used to launch a new vswitch."
+	  default     = "TF_VSwitch"
+}
+variable "vswitch_cidr" {
+	  description = "The cidr block used to launch a new vswitch."
+	  default     = "172.16.1.0/24"
+}
+############### SG ##################
+variable "security_group" {
+		default = "terraform_SG"
+}
+############  Image ######################
+variable "count" {
+  default = "1"
+}
+variable "count_format" {
+  default = "%02d"
+}
+variable "most_recent" {
+  default = true
+}
+variable "image_owners" {
+  default = "marketplace"
+}
+variable "name_regex" {
+  default = "^Magento*"
+}
+variable "role" {
+  default = "magento"
+}
+variable "datacenter" {
+  default = "Frankfurt"
+}
+variable "ecs_type" {
+  default = "ecs.t5-c1m1.large"
+}
+variable "ecs_password" {
+  default = "test12345"
+}
+variable "availability_zones" {
+  default = "eu-central-1a"
+}
+variable "internet_charge_type" {
+  default = "PayByTraffic"
+}
+variable "internet_max_bandwidth_out" {
+  default = 1
+}
+variable "disk_category" {
+  default = "cloud_ssd"
+}
+variable "disk_size" {
+  default = "40"
+}
+/*
+#variable "ecs_id2"{
+	default = "opensuse_42_03_64_20G_alibase_20171031.vhd"
+}
+*/
+variable "ecs_id" {
+	default = "m-gw8azd6ceetnqxll05o7"
+}
+################# RDS ############################
+variable "engine" {
+  default = "MySQL"
+}
+
+variable "engine_version" {
+  default = "5.6"
+}
+
+variable "instance_class" {
+  default = "rds.mysql.t1.small"
+}
+
+variable "storage" {
+  default = "10"
+}
+
+variable "net_type" {
+  default = "Intranet"
+}
+
+variable "user_name" {
+  default = "rdsaccount"
+}
+
+variable "password" {
+  default = "Test12345"
+}
+
+variable "database_name" {
+  default = "databaseMagento"
+}
+
+variable "database_character" {
+  default = "utf8"
+}
