d060790ee408eaccaa26df1915b37df16c437d0a
wojcieh.net <register@wojcieh.net>
2018-10-30 14:16:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,124 +1,116 @@
 ######### Access Key ###############
 variable "access_key" {
-	  type = "string"
-	  default = "XXX"
+	type = "string"
+	default = "XXX"
 }
 variable "secret_key" {
-	  type = "string"
-	  default = "XXX"
+	type = "string"
+	default = "XXX"
 }
 variable "region" {
-	  type = "string"
-	  default = "eu-central-1"
+	type = "string"
+	default = "eu-central-1"
 }
 ################# VPC ############
 variable "vpc_id"{
-		description = "The VPC ID to launch vSwitch"
-		default = ""
+	description = "The VPC ID to launch vSwitch"
+	default = ""
 }
 variable "vpc_name"{
-		description = "VPC Name is TF"
-		default = "TerraformVPC"
+	description = "VPC Name is TF"
+	default = "TerraformVPC"
 }
 variable "vpc_cidr" {
-		default  = "172.16.0.0/12"
+	default  = "172.16.0.0/12"
 }	
 variable "vpc_region" {
-		default = "eu-central-1"
+	default = "eu-central-1"
 }
 variable "vpc_availability_zone"{
-		default = "eu-central-1a"
+	default = "eu-central-1a"
 }
 ########### vswitch ################
 variable "vswitch_id" {
-	  description = "The vswitch id used to launch one or more instances."
-	  default     = ""
+	description = "The vswitch id used to launch one or more instances."
+	default     = ""
 }
 variable "vswitch_name" {
-	  description = "The vswitch name used to launch a new vswitch."
-	  default     = "TF_VSwitch"
+	description = "The vswitch name used to launch a new vswitch."
+	default     = "TF_VSwitch"
 }
 variable "vswitch_cidr" {
-	  description = "The cidr block used to launch a new vswitch."
-	  default     = "172.16.1.0/24"
+	description = "The cidr block used to launch a new vswitch."
+	default     = "172.16.1.0/24"
 }
 ############### SG ##################
 variable "security_group" {
-		default = "terraform_SG"
+	default = "terraform_SG"
 }
 ############  Image ######################
 variable "count" {
-  default = "1"
+	default = "1"
 }
 variable "count_format" {
-  default = "%02d"
+	default = "%02d"
 }
 variable "most_recent" {
-  default = true
+	default = true
 }
 variable "image_owners" {
-  default = "marketplace"
+	default = "marketplace"
 }
 variable "name_regex" {
-  default = "^Magento*"
+	default = "^Magento*"
 }
 variable "role" {
-  default = "magento"
+	default = "magento"
 }
 variable "datacenter" {
-  default = "Frankfurt"
+	default = "Frankfurt"
 }
 variable "ecs_type" {
-  default = "ecs.sn2ne.large"
+	default = "ecs.sn2ne.large"
 }
 variable "ecs_password" {
-  default = "test12345"
+	default = "test12345"
 }
 variable "availability_zones" {
-  default = "eu-central-1a"
+	default = "eu-central-1a"
 }
 variable "disk_category" {
-  default = "cloud_ssd"
+	default = "cloud_ssd"
 }
 variable "disk_size" {
-  default = "40"
+	default = "40"
 }
 variable "ecs_id" {
 	default = "m-gw8azd6ceetnqxll05o7"
 }
 ################# RDS ############################
 variable "engine" {
-  default = "MySQL"
+	default = "MySQL"
 }
-
 variable "engine_version" {
-  default = "5.6"
+	default = "5.6"
 }
-
 variable "instance_class" {
-  default = "rds.mysql.t1.small"
+	default = "rds.mysql.t1.small"
 }
-
 variable "storage" {
-  default = "10"
+	default = "10"
 }
-
 variable "net_type" {
-  default = "Intranet"
+	default = "Intranet"
 }
-
 variable "user_name" {
-  default = "rdsaccount"
+	default = "rdsaccount"
 }
-
 variable "password" {
-  default = "Test12345"
+	default = "Test12345"
 }
-
 variable "database_name" {
-  default = "blah"
+	default = "blah"
 }
-
 variable "database_character" {
-  default = "utf8"
+	default = "utf8"
 }
